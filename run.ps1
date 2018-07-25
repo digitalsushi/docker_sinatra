91dace4448faacda6b16d2fd04757a67f6167d9f
mike cross <mikec@digitalsushi.com>
2018-07-25 15:32:05 -0400

    allows us to use a local config file.

--- run.ps1
+++ run.ps1
@@ -1,2 +1,5 @@
-docker run --rm --name my-sinatra-app -p 80:80 --interactive --tty `
-   --entrypoint /bin/bash digitalsushi/sinatra:latest
+
+Get-Content "developer_config.txt" | foreach-object -begin {$h=@{}} -process { $k = [regex]::split($_,'='); if(($k[0].CompareTo("") -ne 0) -and ($k[0].StartsWith("[") -ne $True)) { $h.Add($k[0], $k[1]) } }
+
+docker run  -v "$h.host_web_directory:/webstuff" --rm --name my-sinatra-app `
+   -p 80:80 --interactive --tty --entrypoint /bin/bash digitalsushi/sinatra:latest
